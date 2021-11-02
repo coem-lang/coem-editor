0bb96773536963791b750e1a95c16b9164074dc8
Katherine Yang <whykatherine@gmail.com>
2021-11-02 15:41:52 -0700

    feat: load example files

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,82 @@
-const buttons = document.querySelectorAll(".nav__item > button");
-let isMenuOpen = false;
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    isMenuOpen = button.parentElement.classList.toggle("nav__item--open");
-  });
-  button.addEventListener("mouseover", () => {
-    if (isMenuOpen) {
-      for (let b of buttons) {
-        b.parentElement.classList.remove("nav__item--open");
-      }
-      button.parentElement.classList.add("nav__item--open");
-      button.focus();
-    }
-  })
-  button.addEventListener("blur", () => {
-    button.parentElement.classList.remove("nav__item--open");
-  })
-})
+const menuBtns = document.querySelectorAll(".nav__item > button"),
+  outputArea = document.querySelector(".output"),
+  filenameTitle = document.querySelector("h2 .filename-title"),
+  tocBtns = document.querySelectorAll(".toc__item button");
+let editor,
+  isMenuOpen = false,
+  currentTitle;
 
-// set up editor
-let editor = CodeMirror(document.querySelector(".editor"), {
-  lineNumbers: true,
-  styleActiveLine: true,
-  styleSelectedText: true,
-  value: 'let me|I;\n\
-let me be "looking";\n\
-print I am "found";\n\
-\n\
-let se(e|a) be "blue";\n\
-print see + " " + sea;\n\
-\n\
-let mis(t|sed) be "thick";\n\
-print mist + " " + missed;\n\
-\n\
-let mou?rning be "dark";\n\
-print morning is mourning;\n\
-\n\
-let answer be nothing;\n\
-print answer;\n\
-let answer be "this";\n\
-print answer;\n\
-let answer be "that";\n\
-print answer;\n\
-let answer be 42;\n\
-print answer;\n\
-\n\
-let you be "here";\n\
-let answer be you;\n\
-print answer;',
+// MENU
+
+function onMenuBtnClick(e) {
+  e.preventDefault();
+  isMenuOpen = e.target.parentElement.classList.toggle("nav__item--open");
+}
+
+function onMenuBtnMouseover(e) {
+  e.preventDefault();
+  if (isMenuOpen) {
+    for (let b of menuBtns) {
+      b.parentElement.classList.remove("nav__item--open");
+    }
+    e.target.parentElement.classList.add("nav__item--open");
+    e.target.focus();
+  }
+}
+
+function onMenuBtnBlur(e) {
+  e.preventDefault();
+  e.target.parentElement.classList.remove("nav__item--open");
+}
+
+// TOC
+
+function onTocBtnClick(e) {
+  e.preventDefault();
+  let filename = e.target.value;
+  load(filename);
+}
+
+// LOAD
+
+function load(filename) {
+  Promise.all([
+    fetch("examples/" + filename + ".poegram"),
+    fetch("examples/" + filename + ".output")
+  ])
+  .then(results => Promise.all(results.map(r => r.text())))
+  .then(([poegram, output]) => {
+    editor.getDoc().setValue(poegram);
+    outputArea.innerText = output;
+    filenameTitle.innerText = filename;
+    currentTitle = filenameTitle;
+    for (let btn of tocBtns) {
+      btn.parentElement.classList.remove("current");
+    }
+    document.querySelector("button[value=\"" + filename + "\"]").parentElement.classList.add("current");
+  })
+  .catch(err => console.log(err));
+}
+
+// MAIN
+
+document.addEventListener("DOMContentLoaded", () => {
+  // set up editor
+  editor = CodeMirror(document.querySelector(".editor"), {
+    lineNumbers: true,
+    styleActiveLine: true,
+    styleSelectedText: true
+  });
+
+  load("Looking");
+
+  menuBtns.forEach(button => {
+    button.addEventListener("click", onMenuBtnClick);
+    button.addEventListener("mouseover", onMenuBtnMouseover);
+    button.addEventListener("blur", onMenuBtnBlur);
+  });
+
+  tocBtns.forEach(btn => {
+    btn.addEventListener("click", onTocBtnClick);
+  });
 });
