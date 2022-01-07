ce28b93091139e2d866b4111db7e2c4fa4e244e1
Katherine Yang <whykatherine@gmail.com>
2022-01-07 13:23:17 -0800

    feat: clean up editor

--- codemirror/theme.js
+++ codemirror/theme.js
@@ -3,5 +3,12 @@ import { EditorView } from "@codemirror/basic-setup";
 export const myTheme = EditorView.baseTheme({
   ".cm-content": {
     fontFamily: "IBM Plex Serif"
+  },
+  "&": {
+    width: "500px",
+    height: "300px"
+  },
+  ".cm-scroller": {
+    overflow: "auto"
   }
 });
