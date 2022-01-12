0afa287f8e88b6e145d0e80aa495ca736de2ab5e
Katherine Yang <whykatherine@gmail.com>
2022-01-11 19:38:34 -0800

    style: gutter

--- codemirror/theme.js
+++ codemirror/theme.js
@@ -10,5 +10,15 @@ export const myTheme = EditorView.baseTheme({
   },
   ".cm-scroller": {
     overflow: "auto"
+  },
+  ".cm-gutters": {
+    color: "#000 !important",
+    background: "none !important",
+    borderRight: "0 !important",
+    fontSize: "12px",
+    fontFamily: "Georgia"
+  },
+  ".cm-gutterElement": {
+    padding: "2px 5px 0 5px !important"
   }
 });
