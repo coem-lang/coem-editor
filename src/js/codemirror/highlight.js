72077bddbebdc0154a76158d6f220bec73a1752f
Katherine Yang <yang110@usc.edu>
2022-04-23 23:26:38 -0700

    feat: use roman serif for bool and null

--- src/js/codemirror/highlight.js
+++ src/js/codemirror/highlight.js
@@ -1,7 +1,10 @@
 import { HighlightStyle, tags } from '@codemirror/highlight';
 
 export const myHighlightStyle = HighlightStyle.define([
-  {tag: [tags.keyword, tags.bool, tags.null],
+  // {tag: [tags.keyword, tags.bool, tags.null],
+  {tag: [tags.bool, tags.null],
+    fontFamily: "IBM Plex Serif"},
+  {tag: tags.keyword,
     fontStyle: "italic"},
   {tag: [tags.string, tags.deleted],
     fontFamily: "IBM Plex Sans"},
