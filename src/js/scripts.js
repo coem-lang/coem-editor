e15a5ce08af5bbf8c719bc1f05b9fc819a30caa8
Katherine Yang <yang110@usc.edu>
2022-04-28 03:49:37 -0700

    feat: mobile styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -151,3 +151,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   reflectBtn.addEventListener("click", reflect);
 });
+
+// NAV
+
+const navToggle = document.querySelector(".navToggle");
+const nav = document.querySelector(".nav");
+
+navToggle.addEventListener("click", e => {
+  e.preventDefault();
+  if (nav.classList.toggle("open")) {
+    navToggle.innerText = "×";
+  } else {
+    navToggle.innerText = "☰";
+  }
+  document.body.classList.toggle("navIsOpen");
+});
