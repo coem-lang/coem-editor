c612bf10bedf06e2099667c3806b5cbaf81c556f
Katherine Yang <katherine@fathom.info>
2022-07-29 13:57:44 -0400

    feat: theme styles

--- src/js/codemirror/theme.js
+++ src/js/codemirror/theme.js
@@ -13,15 +13,16 @@ export const myTheme = EditorView.baseTheme({
     overflow: "auto"
   },
   ".cm-gutters": {
-    color: "#000 !important",
     background: "none !important",
     borderRight: "0 !important",
     fontSize: "12px",
-    fontFamily: "Georgia", // Georgia for oldstyle figures, until implemented in IBM Plex https://github.com/IBM/plex/issues/418
-    lineHeight: "26px"
+    // fontFamily: "Georgia", // Georgia for oldstyle figures, until implemented in IBM Plex https://github.com/IBM/plex/issues/418
+    fontFamily: 'IBM Plex Serif',
+    lineHeight: "28px"
   },
   ".cm-gutterElement": {
-    padding: "2px 20px 0 5px !important"
+    padding: "2px 20px 0 5px !important",
+    color: "#646464"
   },
   ".cm-selectionMatch": {
     backgroundColor: "#e9e8f2 !important"
